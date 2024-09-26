c2dabc77d839959af650b90aefbade47ec14652f
Starozytny <starozytny@hotmail.fr>
2024-09-26 19:23:11 +0200

    to top

--- assets/app/js/app.js
+++ assets/app/js/app.js
@@ -54,3 +54,11 @@ function inputPassword () {
         })
     }
 }
+
+let totop = document.querySelector('.to-top');
+if(totop){
+    totop.addEventListener('click', () => {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    })
+}
