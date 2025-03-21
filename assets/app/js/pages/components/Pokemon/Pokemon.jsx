51acca0dc0e58370b02876e904df8e6e4c2073e9
shanbora <chanbora@logilink.fr>
2025-03-21 16:58:09 +0100

    new pokemon

--- assets/app/js/pages/components/Pokemon/Pokemon.jsx
+++ assets/app/js/pages/components/Pokemon/Pokemon.jsx
@@ -309,7 +309,7 @@ const borderColors = {
 
 const types = ["fire", "water", "grass", "electric", "psychic", "ice", "dragon", "dark", "fairy", "normal", "fighting", "flying", "poison", "ground", "rock", "bug", "ghost", "steel"]
 
-const specials = ["chhaydarong", "chanda", "chloe", "marie", "riddthy", "sophia", "oceane", "mike", "florian", "manon", "fiona"]
+const specials = ["chhaydarong", "chanda", "chloe", "marie", "riddthy", "sophia", "oceane", "mike", "florian", "manon", "fiona", "john", "kevin", "sergio", "chhay", "yoan"]
 
 function getRandomInt(max) {
 	return Math.floor(Math.random() * max);
