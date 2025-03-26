ed2d516d3611c6294293863f982e9429bbeb803b
shanbora <chanbora@logilink.fr>
2025-03-26 15:09:05 +0100

    marguerite

--- src/Controller/App/BlogController.php
+++ src/Controller/App/BlogController.php
@@ -12,11 +12,20 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/aventures', name: 'app_adventures_')]
 class BlogController extends AbstractController
 {
-    // LAST ID 10
+    // LAST ID 11
 
     public function __construct(private readonly BoCommentaryRepository $commentaryRepository,
                                 private readonly BoViewRepository $viewRepository)
     {}
+
+    #[Route('/lac-sainte-marguerite', name: 'marguerite')]
+    public function marguerite(): Response
+    {
+        if(!$this->isGranted('ROLE_ADMIN')) {
+            $this->stat(11);
+        }
+        return $this->render('app/pages/blog/2024/sainte_marguerite.html.twig', $this->getCommentaries(11));
+    }
 
     #[Route('/plateau-emparis', name: 'emparis')]
     public function emparis(): Response
