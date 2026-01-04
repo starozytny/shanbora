44d8985086032d01ca31ce6dfa5db705f057b5b9
Shanbora <starozytny@hotmail.fr>
2026-01-04 13:56:00 +0100

    list albums for users

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Main\Gallery\GaAlbum;
 use App\Entity\Main\User;
 use App\Repository\Main\Gallery\GaAlbumRepository;
+use App\Repository\Main\Gallery\GaImageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,18 +14,42 @@ use Symfony\Component\Routing\Attribute\Route;
 class UserController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(GaAlbumRepository $albumRepository): Response
+    public function index(GaAlbumRepository $albumRepository, GaImageRepository $imageRepository): Response
     {
         /** @var User $user */
         $user = $this->getUser();
 
         $userId = $user->getId();
         $albums = $albumRepository->findBy([], ['dateAt' => 'DESC']);
+        $photos = $imageRepository->findAll();
+
+        $albums = array_filter($albums, function ($entity) use ($userId) {
+            return ($entity->getCanAccess() && in_array($userId, $entity->getCanAccess())) || $entity->getUser()->getId() === $userId;
+        });
+
+        $data = []; $totalAlbums = count($albums); $totalPhotos = 0; $totalYears = 0;
+        foreach($albums as $album) {
+            $tmp = $data[$album->getDateAt()->format('Y')] ?? null;
+            if($tmp){
+                $data[$album->getDateAt()->format('Y')][] = $album;
+            }else{
+                $data[$album->getDateAt()->format('Y')] = [$album];
+                $totalYears++;
+            }
+        }
+
+        $indexPhotos = [];
+        foreach($photos as $photo){
+            $indexPhotos[$photo->getAlbum()->getId()][] = $photo;
+            $totalPhotos++;
+        }
 
         return $this->render('user/pages/index.html.twig', [
-            'albums' => array_filter($albums, function ($entity) use ($userId) {
-                return ($entity->getCanAccess() && in_array($userId, $entity->getCanAccess())) || $entity->getUser()->getId() === $userId;
-            })
+            'data' => $data,
+            'indexPhotos' => $indexPhotos,
+            'totalAlbums' => $totalAlbums,
+            'totalPhotos' => $totalPhotos,
+            'totalYears' => $totalYears,
         ]);
     }
 
